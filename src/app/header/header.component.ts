d06ad55dacf85088dd4ac8b632c9383765798994
tawsifmahmud05 <tawsifmahmud05@gmail.com>
2024-06-10 15:07:14 +0600

    first

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  //   template: '<h1>Hello World</h1>',
+  //   styles: ['h1{color:red}'],
+  //   styleUrls: ['./header.component.css'],
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
